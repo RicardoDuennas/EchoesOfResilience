c07a7753a706b91167e080c7906c5e296d446518
Ric <ricardoduennas@gmail.com>
2024-08-24 13:36:35 -0500

    Implemented throwable skulls to the map

--- Assets/Scripts/Puzzles/Target.cs
+++ Assets/Scripts/Puzzles/Target.cs
@@ -29,13 +29,22 @@ public class Target : MonoBehaviour
         {
             //BasicEnemy enemy = collision.gameObject.GetComponent<BasicEnemy>();
             GameObject dart = collision.gameObject;
+            dart.tag = "DartAttached";
 
             rb = dart.GetComponent<Rigidbody>();
             // make sure projectile sticks to surface
+            Debug.Log(rb.isKinematic);
             rb.isKinematic = true;
+           Debug.Log(rb.isKinematic);
+ 
+            // Vector3 tempPos = rb.transform.position;
+            // Debug.Log(rb.transform.position);
+            
+            // // make sure projectile moves with target
+            // rb.transform.SetParent(this.transform, false); 
+            // //rb.transform.position = tempPos;
 
-            // make sure projectile moves with target
-            transform.SetParent(this.transform); 
+            // Debug.Log(rb.transform.position);
 
         }
     }
