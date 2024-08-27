b8b026ba2fd11b6604a17ebcafba8f90efdaa003
Ric <ricardoduennas@gmail.com>
2024-08-27 15:56:56 -0500

    Update Target.cs

--- Assets/Scripts/Puzzles/Target.cs
+++ Assets/Scripts/Puzzles/Target.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using PrimeTween;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class Target : MonoBehaviour
@@ -14,7 +15,8 @@ public class Target : MonoBehaviour
     DartReceiver moonScript;
     DartReceiver sunScript;
     DartReceiver americaScript;
-    [SerializeField] private bool[] darts = new bool[3];
+    GameObject dart;
+    [SerializeField] private bool[] targets = new bool[3];
     public int damage;
 
     void Awake()
@@ -23,9 +25,9 @@ public class Target : MonoBehaviour
         moonScript = GameObject.Find("Moon").GetComponent<DartReceiver>(); 
         sunScript = GameObject.Find("Sun").GetComponent<DartReceiver>(); 
         americaScript = GameObject.Find("America").GetComponent<DartReceiver>(); 
-        darts[0] = false;
-        darts[1] = false;
-        darts[2] = false;
+        targets[0] = false;
+        targets[1] = false;
+        targets[2] = false;
     }
 
     // Update is called once per frame
@@ -39,21 +41,27 @@ public class Target : MonoBehaviour
         // check if you hit an enemy
         if(collision.gameObject.CompareTag("Dart"))
         {
+            Debug.Log("1");
+
             bool hit = false;
 
-            if (moonScript.success && !darts[0])
-            {   darts[0] = true;
+            if (moonScript.success && !targets[0])
+            {   targets[0] = true;
                 hit = true;
-            } else if (sunScript.success  && !darts[1]){
-                darts[1] = true;
+            Debug.Log("2");
+            } else if (sunScript.success  && !targets[1]){
+                targets[1] = true;
                 hit = true;
-            } else if (americaScript.success && !darts[2]){
-                darts[2] = true;
+            Debug.Log("3");
+            } else if (americaScript.success && !targets[2]){
+                targets[2] = true;
                 hit = true;
+            Debug.Log("4");
             }
             
             if (hit){
-                GameObject dart = collision.gameObject;
+                Debug.Log("5");
+                dart = collision.gameObject;
                 dart.tag = "DartAttached";
 
                 rb = dart.GetComponent<Rigidbody>();
@@ -61,7 +69,8 @@ public class Target : MonoBehaviour
                 rb.isKinematic = true;
                 dart.GetComponent<XRGrabInteractable>().enabled = false;
             } else {
-
+                Debug.Log("6");
+                Tween.Position(dart.transform, endValue: new Vector3(-53.3899994f,1.56799996f,-4.92799997f), duration: 3);
             }
 
         }
